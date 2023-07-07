1273ae8c39639d11e4960a4b9095c7a48e110d2f
Guilliano van Dinther <guilliano.vandinther@ictmbo.nl>
2023-07-07 21:12:15 +0200

    update

--- JavaScript/reddit/revealSpoilers.js
+++ JavaScript/reddit/revealSpoilers.js
@@ -3,21 +3,19 @@ const targetDiv = document.querySelector('.rpBJOHq2PR60pnwJlUyP0');
 const observer = new MutationObserver((mutationsList) => {
     for (let mutation of mutationsList) {
         if (mutation.type === 'childList') {
-            // Check if the added node is a <div>
             const addedNodes = Array.from(mutation.addedNodes);
             const addedDivs = addedNodes.filter(node => node.tagName && node.tagName.toLowerCase() === 'div');
 
-            // Perform actions for the added <div> elements
             addedDivs.forEach(div => {
                 if (div.querySelector('span[style="border: 1px solid rgb(164, 167, 168); color: rgb(164, 167, 168);"]')) {
-                    if (div.querySelector('a[data-testid="outbound-link"]')) {
+                    if (div.querySelector('a[data-testid="outbound-link"]') && !div.querySelector('a[data-testid="outbound-link"]').getAttribute('href').includes('reddit')) {
                         const noBlurLink = div.querySelector('a[data-testid="outbound-link"]').getAttribute('href');
                         const blurredImage = div.querySelector('img[alt="Post image"]');
 
                         blurredImage.setAttribute('src', noBlurLink);
-                    } else {
+                    } else if (div.querySelector('.m3aNC6yp8RrNM_-a0rrfa img:first-child')){
                         const blurredImage = div.querySelector('img[alt="Post image"]');
-                        const url = div.querySelector('div[data-click-id="media"] > img');
+                        const url = div.querySelector('.m3aNC6yp8RrNM_-a0rrfa img:first-child');
                         const urlText = url.getAttribute('src');
                         const match = urlText.match(/\/([^\/?]+)\?blur/);
 
