9eb885b04a818628cb17d9a6eaa02d2e3db06b05
Guilliano van Dinther <guilliano.vandinther@ictmbo.nl>
2024-02-07 10:00:39 +0100

    fixed side panel not covering fully

--- JavaScript/ytmusic/removeSongImage.js
+++ JavaScript/ytmusic/removeSongImage.js
@@ -1,4 +1,4 @@
-const css = '#main-panel { display: none !important; } #player-page>div>div.side-panel.modular.style-scope.ytmusic-player-page, #player-page>div>div.side-panel.modular.style-scope.ytmusic-player-page>tp-yt-paper-tabs, #tab-renderer { margin: 0 !important; width: 100vw !important; }';
+const css = '#main-panel { display: none !important; } #player-page>div>div.side-panel.modular.style-scope.ytmusic-player-page, #player-page>div>div.side-panel.modular.style-scope.ytmusic-player-page>tp-yt-paper-tabs, #tab-renderer, #side-panel { margin: 0 !important; width: 100vw !important; }';
 const styleElement = document.createElement('style');
 const head = document.head || document.getElementsByTagName('head')[0];
 
